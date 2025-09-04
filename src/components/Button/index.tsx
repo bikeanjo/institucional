9ff46ae4e3246efa20879bc24420f1d6d62a7ce2
mariagmss <166930239+mariagmss@users.noreply.github.com>
2025-09-04 15:54:40 -0300

    adjusted styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,32 +1,43 @@
-import { Button as MuiButton } from "@mui/material";
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from "@mui/material";
 import styled from "styled-components";
+import { LinkProps } from "react-router-dom";
 
-interface ButtonProps {
+interface ButtonProps extends MuiButtonProps {
   center?: boolean;
+  left?: boolean;
+  fullWidthMobile?: boolean;
+  fullWidthDesktop?: boolean;
+  to?: LinkProps["to"];
 }
 
-const Button = styled(MuiButton)<ButtonProps>(({ theme, center }) => ({
-  backgroundColor: "#4D7A18",
-  width: "fit-content",
-  color: "white",
-  fontWeight: 700,
-  fontSize: "16px",
-  borderRadius: "4px",
-  textTransform: "none",
-  padding: "15px 32px 15px 32px",
-  boxShadow: "0px 2px 6px 0px rgba(0, 0, 0, 0.15)",
+const Button = styled(MuiButton)<ButtonProps>(
+  ({ theme, center, left, fullWidthMobile, fullWidthDesktop }) => ({
+    backgroundColor: "#4D7A18",
+    color: "white",
+    fontWeight: 700,
+    fontSize: "16px",
+    borderRadius: "4px",
+    textTransform: "none",
+    padding: "15px 32px 15px 32px",
+    boxShadow: "0px 2px 6px 0px rgba(0, 0, 0, 0.15)",
 
-  "&:hover": {
-    backgroundColor: "#43700E",
-  },
+    "&:hover": {
+      backgroundColor: "#43700E",
+    },
 
-  [theme.breakpoints.up("xs")]: {
-    margin: "0 auto",
-  },
+    [theme.breakpoints.up("xs")]: {
+      width: fullWidthMobile ? "100%" : "fit-content",
+      margin: left ? "0" : "0 auto",
+    },
 
-  [theme.breakpoints.up("lg")]: {
-    margin: center ? "0 auto" : "0",
-  },
-}));
+    [theme.breakpoints.up("lg")]: {
+      width: fullWidthDesktop ? "100%" : "fit-content",
+      margin: center ? "0 auto" : "0",
+    },
+  }),
+);
 
 export default Button;
