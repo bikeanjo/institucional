e9c7f7caee5be287a38fc0cb9d6f7e0254e4f9a0
mariagmss <166930239+mariagmss@users.noreply.github.com>
2025-10-07 17:28:46 -0300

    removed unused component folders

--- src/pages/BikeAnjas/Components/Paragraph/Paragraph.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Paragraph = styled.p`
-  font-size: 16px;
-  text-align: center;
-  color: #000000;
-  margin: 0 auto;
-  line-height: auto;
-`;
